d0d32878fd41be542da5cf95b1da68ccd1b54dcb
ashish <ashishg931@gmail.com>
2019-09-13 02:20:28 +0200

    updated solutions for time complexity.

---
+++ src/main/java/countingelements/FrogRiverOne.java
@@ -0,0 +1,30 @@
+package countingelements;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FrogRiverOne {
+
+	public static void main(String[] args) {
+		int[] input = { 1, 3, 1, 4, 2, 3, 5, 4 };
+		int X = 5;
+		System.out.println("vale:" + solution(X, input));
+	}
+
+	public static int solution(int X, int[] A) {
+		int flag = X;
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < A.length; i++) {
+			if ((A[i] <= X) && !map.containsKey(A[i])) {
+				map.put(A[i], i);
+				flag--;
+			}
+		}
+		if (!map.containsKey(X) || flag != 0)
+			return -1;
+		else
+			return Collections.max(map.values());
+	}
+}
---
+++ src/main/java/countingelements/PermCheck.java
@@ -0,0 +1,24 @@
+package countingelements;
+
+import java.util.Arrays;
+
+public class PermCheck {
+
+	public static void main(String[] args) {
+		int[] input = {  1, 4, 3 };
+		System.out.println(solution(input));
+	}
+
+	public static int solution(int[] A) {
+		Arrays.sort(A);
+
+		int count = 1;
+		for (int i = 0; i < A.length; i++) {
+			if (count != A[i])
+				return 0;
+			count++;
+		}
+		return 1;
+	}
+
+}
